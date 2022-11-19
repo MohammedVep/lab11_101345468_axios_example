d15a8dea0b6edef350cb5e79b4856721180be759
MohammedVep <98326062+MohammedVep@users.noreply.github.com>
2022-11-19 06:52:37 -0500

    Problem

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+import react from 'react';
+import PersonList from './PersonList.js';
+import  Component  from 'react';
+function App() {
+  return (
+    <>
+      <PersonList />
+    </>
+  );
+}
+
+export default App;
---
+++ src/PersonList.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import axios from 'axios'
+import { Component } from 'react';
+import {useState } from "react";
+export default class PersonList extends Component {
+  
+     //Define state default values
+     state = {
+        persons: []
+    }
+
+     //Component Lifecycle Callback
+    componentDidMount(){
+        axios.get(`https://randomuser.me/api/?results=10`)
+        .then(res => {
+            console.log(res.data);
+            const persons = res.data.results;
+            this.setState({ persons });
+        });
+    }
+  render() {
+    return (
+      <>
+        <div>
+          <h1>PersonList</h1>
+          <ul>
+              {
+                  this.state.persons
+                    .map(person => 
+                      <li key={person.id}>{person.name}</li>)
+
+              }
+          </ul>
+        </div>
+     </>
+    );
+  }
+}
